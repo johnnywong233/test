dd24e25f6926ea1c45013c03c4fd1f9e68ca9478
johnny <1224017485@qq.com>
2017-07-12 22:23:54 +0800

    add simple and silly spring boot velocity demo

---
+++ springboot_demo/springboot_velocity/src/main/java/velocity/App.java
@@ -0,0 +1,17 @@
+package velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Johnny
+ * Date: 2017/07/12
+ * Time: 22:50
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
