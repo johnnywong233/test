a7435ffd131e62cf5de1d2f0d21119ee1c479ebb
johnny <1224017485@qq.com>
2017-08-05 00:37:11 +0800

    build war with tomcat, not embedded tomcat jar

---
+++ springboot_demo/springboot_velocity/src/main/java/velocity/ServletInitializer.java
@@ -0,0 +1,16 @@
+package velocity;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Author: Johnny
+ * Date: 2017/8/5
+ * Time: 0:21
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+}
