dbf0362b9579f823aa12c40ff832bb36f4f533ab
johnny <jian.wang15@hpe.com>
2017-10-18 00:30:06 +0800

    add spring cloud api gateway learning case

---
+++ springcloud_demo/springcloud_api_gateway/src/main/java/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Author: Johnny
+ * Date: 2017/10/17
+ * Time: 12:28
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
