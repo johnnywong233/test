cf1e3134d49864ebdc05a081ea390053cbe14d1d
王健 <wangjian@lattebank.com>
2018-01-31 23:45:16 +0800

    add

---
+++ springcloud_demo/springcloud_feign_upload/src/main/java/feign/App.java
@@ -0,0 +1,14 @@
+package feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Johnny on 2018/1/31.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ springcloud_demo/springcloud_feign_upload/src/main/java/feign/UploadFeignClient.java
@@ -0,0 +1,31 @@
+package feign;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by Johnny on 2018/1/31.
+ */
+@FeignClient(name = "ms-content-sample", configuration = UploadFeignClient.MultipartSupportConfig.class)
+public interface UploadFeignClient {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST,
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    String handleFileUpload(@RequestPart(value = "file") MultipartFile file);
+
+    class MultipartSupportConfig {
+        @Bean
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
+}
