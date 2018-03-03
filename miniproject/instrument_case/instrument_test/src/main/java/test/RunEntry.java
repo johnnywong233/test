ec513cec8946394a8eeddbf643841c5125a6814b
王健 <wangjian@lattebank.com>
2018-03-03 23:57:13 +0800

    add instrument learning case

---
+++ miniproject/instrument_case/instrument_test/src/main/java/test/RunEntry.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created by Johnny on 2018/3/3.
+ */
+public class RunEntry {
+    public static void main(String[] args) {
+        new RunEntry().sayHello();
+    }
+
+    private void sayHello() {
+        System.out.println("Hello, johnny!");
+    }
+}
