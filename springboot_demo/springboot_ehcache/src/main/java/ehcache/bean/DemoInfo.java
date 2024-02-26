cbda19e0c22af0ad3ca119c743ee8235e95920a5
johnny <jian.wang@adachina.com>
2024-02-26 17:17:37 +0800

    fix:CommandAcceptanceException: Error executing DDL "create table hibernate_sequence (next_val bigint) engine=MyISAM" via JDBC Statement

--- springboot_demo/springboot_ehcache/src/main/java/ehcache/bean/DemoInfo.java
+++ springboot_demo/springboot_ehcache/src/main/java/ehcache/bean/DemoInfo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 public class DemoInfo {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String pwd;
