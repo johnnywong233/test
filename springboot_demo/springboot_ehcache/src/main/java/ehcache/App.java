03d20ec1150e99babe3dc09214230bda98041950
johnny <jian.wang@adachina.com>
2024-02-26 17:14:07 +0800

    建表脚本

--- springboot_demo/springboot_ehcache/src/main/java/ehcache/App.java
+++ springboot_demo/springboot_ehcache/src/main/java/ehcache/App.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * create table demo_info (
+ * id    serial primary key,
+ * name  varchar(255),
+ * pwd   varchar(255),
+ * state integer not null
+ * );
  * Author: Johnny
  * Date: 2017/4/17
  * Time: 23:35
