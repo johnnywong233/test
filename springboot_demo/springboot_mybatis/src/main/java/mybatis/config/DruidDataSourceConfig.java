97b092316cc19264d8858b90e2687b30595fe66f
johnny <jian.wang@adachina.com>
2024-02-26 15:09:40 +0800

    RelaxedPropertyResolver升级

--- springboot_demo/springboot_mybatis/src/main/java/mybatis/config/DruidDataSourceConfig.java
+++ springboot_demo/springboot_mybatis/src/main/java/mybatis/config/DruidDataSourceConfig.java
@@ -1,46 +1,50 @@
 package mybatis.config;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
+@Slf4j
 @Configuration
 @EnableTransactionManagement
 public class DruidDataSourceConfig implements EnvironmentAware {
 
-    private RelaxedPropertyResolver propertyResolver;
+    private Properties properties;
 
     @Override
-    public void setEnvironment(Environment env) {
-        this.propertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
+    public void setEnvironment(@NonNull Environment env) {
+        this.properties = Binder.get(env)
+                .bind("spring.datasource.", Properties.class)
+                .orElse(null);
     }
 
     @Bean
     public DataSource dataSource() {
-        System.out.println("inject druid.");
+        log.info("inject druid.");
         DruidDataSource datasource = new DruidDataSource();
-        datasource.setUrl(propertyResolver.getProperty("url"));
-        datasource.setDriverClassName(propertyResolver.getProperty("driver-class-name"));
-        datasource.setUsername(propertyResolver.getProperty("username"));
-        datasource.setPassword(propertyResolver.getProperty("password"));
-        datasource.setInitialSize(Integer.valueOf(propertyResolver.getProperty("initial-size")));
-        datasource.setMinIdle(Integer.valueOf(propertyResolver.getProperty("min-idle")));
-        datasource.setMaxWait(Long.valueOf(propertyResolver.getProperty("max-wait")));
-        datasource.setMaxActive(Integer.valueOf(propertyResolver.getProperty("max-active")));
-        datasource.setMinEvictableIdleTimeMillis(Long.valueOf(propertyResolver.getProperty("min-evictable-idle-time-millis")));
+        datasource.setUrl(properties.getProperty("url"));
+        datasource.setDriverClassName(properties.getProperty("driver-class-name"));
+        datasource.setUsername(properties.getProperty("username"));
+        datasource.setPassword(properties.getProperty("password"));
+        datasource.setInitialSize(Integer.parseInt(properties.getProperty("initial-size")));
+        datasource.setMinIdle(Integer.parseInt(properties.getProperty("min-idle")));
+        datasource.setMaxWait(Long.parseLong(properties.getProperty("max-wait")));
+        datasource.setMaxActive(Integer.parseInt(properties.getProperty("max-active")));
+        datasource.setMinEvictableIdleTimeMillis(Long.parseLong(properties.getProperty("min-evictable-idle-time-millis")));
         try {
             datasource.setFilters("stat, wall");
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("dataSource init fail", e);
         }
         return datasource;
     }
